cc45a6ba1b517dfe1ae085b761a51e8156bff2bc
akcel123 <akcel322@gmail.com>
2023-07-10 16:36:11 +0700

    some fix

--- MyTrainer.Domain/Training.cs
+++ MyTrainer.Domain/Training.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace MyTrainer.Domain;
 
-namespace MyTrainer.Domain
+internal class Training
 {
-    internal class Training
-    {
-    }
+    public Guid UserId { get; set; }        // ID пользователя, которому предназначена тренировка
+    public Guid TrainerId { get; set; }     // Вот это свойство под вопросом (ID тренера, который написал тренировку)
+    public Guid Id { get; set; }            // ID самой тренировки
+    public string? Name { get; set; }       // Название тренировки (например, "силовая тренировка ног")
+    public string Description { get; set; } // Определяет всю тренировку, просто текстовым файлом
+    public DateTime CreationDate { get; set; }
+    public DateTime? EditDate { get; set; }
+    public bool IsCompleted { get; set; }    
 }
